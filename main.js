e874e8c6930e21c42f8cecba516c48ef84c1feaa
Thelpix <ivan123456274@gmail.com>
2020-12-01 21:41:38 -0300

    commit

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-window.onload = randomimg;
-
-var pic = new Array ("img/gif/4dCube1.gif","img/gif/4dCube2.gif", "img/gif/4dCube3.gif", "img/gif/4dCube4.gif", "img/gif/4dCube5.gif", "img/gif/4dCube6.gif");
-
 function randomimg() {
-    var num = Math.floor(Math.random()* pic.length);
-    document.getEelementById("Geom").src = pic[num];
+    var num = Math.floor((Math.random()*6)+1);
+    document.getElementById("Geom").innerHTML=
+        '<img src="img/gif/4dCube${num}.gif">';
 }
