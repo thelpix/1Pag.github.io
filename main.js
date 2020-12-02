9539ffe1c2f2b50c560b56388ecad80d22ef48c7
Thelpix <ivan123456274@gmail.com>
2020-12-02 12:09:00 -0300

    Imagenes aleatorias
    Añadi que:
     -Sea aleatorio la imagen al refrescar.
     -El boton "Nuevo Objeto", pone la siguiente imagen

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-function randomimg() {
-    var num = Math.floor((Math.random()*6)+1);
-    document.getElementById("Geom").innerHTML=
-        '<img src="img/gif/4dCube${num}.gif">';
+//var myPic = ("/4dCube1.gif","/4dCube2.gif","/4dCube3.gif","/4dCube4.gif","/4dCube5.gif","/4dCube6.gif");
+var num = Math.floor((Math.random() * 5)+1 );
+function getRandomImage() 
+{
+    var imgStr = `<img src="img/gif/4dCube${num}.gif" alt = "4d Object"></img>`;
+    document.write(imgStr); document.close();
+}
+
+function buttonimg() 
+{
+    if(num > 6)  
+    {
+        num = 1;
+    }
+    document.getElementById('Geom').innerHTML = 
+    `<img src="img/gif/4dCube${num++}.gif" alt = "4d Object"></img>`;
 }
